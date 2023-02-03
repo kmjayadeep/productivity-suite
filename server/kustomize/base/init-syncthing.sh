1b34b28bbdd5f9cb34400eb283da9f6dc4649eea
Jayadeep KM <kmjayadeep@gmail.com>
2023-02-03 07:36:41 +0100

    add check for snapshots
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- server/kustomize/base/init-syncthing.sh
+++ server/kustomize/base/init-syncthing.sh
@@ -8,6 +8,12 @@ BASE=/syncthing
 CONFIG_DIR=/syncthing/config
 DATA_DIR=/syncthing/data
 
+l=$(restic snapshots --latest 1 --json)
+if [ "$l" = "[]" ]; then
+  echo "No previous backups found, ignoring restore"
+  exit 0
+fi
+
 if [ "$(ls -A $CONFIG_DIR)" ]; then
  echo "$CONFIG_DIR is not empty, all good"
 else
