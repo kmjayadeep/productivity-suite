9433f5a830f3f77cdd12d0de5bc696d1ff6bd236
Jayadeep KM <kmjayadeep@gmail.com>
2023-08-17 11:50:33 +0530

    fix restic schedule
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- server/kustomize/base/backup.sh
+++ server/kustomize/base/backup.sh
@@ -5,11 +5,10 @@ set -e
 while true;
 do
   echo "backing up";
-  restic --verbose backup /data
-  restic --verbose backup /config
+  restic --verbose backup /data /config
   echo "backup successful"
   echo "cleaning up old snapshots"
-  restic forget --keep-monthly 12 --keep-last 7 --keep-daily 10 --prune
+  restic forget --keep-monthly 6 --keep-last 7 --keep-daily 10 --prune
   echo "current snapshots:"
   restic snapshots
   sleep 3600
